52eb66ed3c76ed684c3ace1efe0b26f8705f90e6
Tilthood <119104036+Tilthood@users.noreply.github.com>
2022-11-26 16:37:17 +0100

    Implément de la fonction ajouter une formation
    Ajout des menus permettant l'implémentation d'une nouvelle formation.
    Hf bro

--- Projet.c
+++ Projet.c
@@ -21,111 +21,222 @@ typedef struct formateur {
 
 typedef struct formation {
     char nom[100];
-    char cours[50];
+    char cours[20][50];
     char anneeCours[5][50];
-    int maxEtudiant;
+    int maxEtudiant, nombreAnneeFormation;
     float prix;
     struct formation *suivant;
 }formation;
 
+
+
 int main() {
-//Ouverture des fichiers *dat et *res
+    //Ouverture des fichiers *dat et *res
+    FILE *fdat, *fdat1, *fdat2, *fres, *fres1, *fres2;
+    fres = fopen("listeEtdudiant.dat","w");
+    fres1 = fopen("listeFormateur.dat","w");
+    fres2 = fopen("listeFormation.dat","w");
 
+    fdat = fopen("listeEtdudiant.dat","r");
+    fdat1 = fopen("listeFormateur.dat","r");
+    fdat2 = fopen("listeFormation.dat","r");
 
-//Declaration des variables
-int valeurMenu = 0;
+    //Declaration des variables
+    int valeurMenu, queFaire;
+    //queFaire est une variable qui est modifiée par les fonctions : en effet, on doit naviguer avec les menus, mais la lecture et l'écriture doit se faire dans le
+    //main. (Préférence de M. Carpentier) Donc, si un menu nécessite une lecture/écriture, il faut lui faire varier la valeur de queFaire. Voir valeurMenu == 3 pour exemple
 
-/*------------------------------------------------------Fin declaration des variables----------------------------------------------------------------*/
-void changerMenu(int *);
+    //variables formation
+    formation *formationDebut, *formationCourant, *formationSuivant, *nouvelleFormation;
+    formationCourant = malloc(sizeof(formation));
+    formationDebut = formationCourant;
 
-while(valeurMenu != 0) {
-		
-		if(valeurMenu == 1) {
-	        // fonction consulter horaire
-	        printf("Chargement du menu ""consulter un horaire"" ...");
-	        menuConsulterHoraire();
-	        printf("\nJe quitte la fonction horaire\n");
-	    }
-	
-	    if(valeurMenu == 2) {
-	        // fonction ajouter élève
-	    }
-	
-	    if(valeurMenu == 3) {
-	        // fonction gestion formation
-	    }
-	
-	    if(valeurMenu == 4) {
-	        // fonction consulter etudiant
-	    }
-	    
-	    
-    	menuPrincipal(&valeurMenu);
-	}
+    //!!!! A SUPPRIMER QUAND ON AURA PLUSIEURS FORMATIONS. JUSTE POUR TESTER LA FEATURE
+    formationDebut = NULL;
+    formationDebut->suivant = NULL;
+
+    /*------------------------------------------------------Fin declaration des variables----------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------*/
     
+    // Declaration des fonctions
+    void changerMenu(int *);
+    menuConsulterHoraire();
+    int menuGererFormation();
+    int gestionFormation();
+
+
+    /*------------------------------------------------------Fin declaration des fonctions ---------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------*/
+   
+    //Initialisation de la valeur de valeurMenu
+    changerMenu(&valeurMenu);
+    while(valeurMenu != 0) {
+
+       
+
+        if(valeurMenu == 1) {
+            // fonction consulter horaire
+            menuConsulterHoraire();
+        }
+    
+        if(valeurMenu == 2) {
+            // fonction ajouter élève
+        }
+    
+        if(valeurMenu == 3) {
+            // fonction gestion formation
+           queFaire = menuGererFormation();
+           if(queFaire != 0) {
+                //Soit lire ou écrire une formation/formateur dans un fichier
+                if(queFaire == 1) {
+                    //Ici on sait que l'utilisateur souhaite érire une nouvelle formation.
+                    printf("===================================\n");
+                    //Création d'un espace mémoire pour la nouvelleFormation
+                    nouvelleFormation = malloc(sizeof(formation));
+                    nouvelleFormation->suivant = formationDebut->suivant;
+                    formationDebut->suivant = nouvelleFormation;
+
+                    //Compléter les caractéristiques de la formation
+                    prinft("Quel est le nom de la nouvelle formation ?");
+                    scanf("%s",&nouvelleFormation->nom);
+                    prinft("Quels cours sont donnés dans la nouvelle formation ?"); //pour le moment on ne met que le premier cours, pour test
+                    scanf("%s",&nouvelleFormation->cours[1]);
+                    printf("En combien d'année se réalise la formation ?\n")
+                    scanf("%d", &nouvelleFormation->nombreAnneeFormation);
+                    prinft("Quels cours sont donnés à chaque année ?"); //Complètement skip ca pour le moment, pas le temps de faire les boucles,
+                    //scanf("%s",&nouvelleFormation->);
+                    prinft("Combien d'étudiants peut acceuillir la nouvelle formation ?");
+                    scanf("%d",&nouvelleFormation->maxEtudiant);
+                    prinft("Quel est le prix pour chaque étudiant pour cette formation ?");
+                    scanf("%f",&nouvelleFormation->prix);
+                }
+           }
+        }
+    
+        if(valeurMenu == 4) {
+            // fonction consulter etudiant
+        }
+
+         changerMenu(&valeurMenu);
+        
+    }
+        
+    //Fermeture des flux de lecture et d'écriture
+    printf("Fermeture des fichiers.\n");
+    fclose(fdat);
+    fclose(fdat1);
+    fclose(fdat2);
+    fclose(fres);
+    fclose(fres1);
+    fclose(fres2);
+    printf("Au revoir et a bientot.\n");
+
 }   //Fin du programme principal.
 
 
 //ChangerMenu
-void changerMenu( int *valeurMenu) {
-    /*La valeur du menu principal est 0. Donc, si *valeurMenu est déjà à 0, on propose un affichage pour 0 qui s'appelle 'Retour'.*/
-    if(*valeurMenu == 0) {
-        printf("Veuillez entrer la valeur correspondante au menu souhaite :\n");
-        printf("0 : Retour\n");
-        printf("1 : Consulter un horaire\n");
-        printf("2 : Ajouter un etudiant\n");
-        printf("3 : Gerer les formations\n");
-        printf("4 : Consulter les etudiants\n");
+void changerMenu(int *valeurMenu) {
+    
+    printf("Veuillez entrer la valeur correspondante au menu souhaite :\n");
+    printf("0 : Quitter\n");
+    printf("1 : Acceder aux horaires\n");
+    printf("2 : Ajouter un etudiant\n");
+    printf("3 : Gerer les formations\n");
+    printf("4 : Consulter les etudiants\n");
+    printf("Votre choix : ");
 
+    scanf("%d", &*valeurMenu);
+
+    //Gestion d'erreur
+    while(*valeurMenu < 0 || *valeurMenu > 4) {
+        printf("Valeur incorrecte.\n");
+        printf("Votre choix : ");
         scanf("%d", &*valeurMenu);
-
-        //Gestion d'erreur
-        while(*valeurMenu < 0 || *valeurMenu > 4) {
-            printf("Valeur incorrecte.\n");
-            scanf("%d", &*valeurMenu);
-        }
-
-    } else {
-        /*On est déjà dans un autre menu. Selon la valeur (1 à 4), Faire appel à une fonction propre à ce menu.
-          Car, par exemple, Gérer les formations débouche sur 2 sous menu + possibilités de revenir au menu principal. Donc ce serait le bordel si on met tout en brute ici
-          --> Faire des fonctions sera plus clair
-          !! Ne pas utiliser 2 fois le même chiffre pour un menu différent car chaque sous menu à son chiffre qui lui sert d'ID en quelque sorte !!
-        */
-
-        //pas encore d'autre menu réalisé
     }
 
-    //Appel des fonctions d'affichage correspondant
-    /*Pas encore mise en place pour le moment*/
 }
 
 
-
 void menuConsulterHoraire() {
-    /*La valeur du menu principal est 0. Donc, si *valeurMenu est déjà à 0, on propose un affichage pour 0 qui s'appelle 'Retour'.*/
-
     int valeurMenuHoraire=-1;
-
-    while(valeurMenuHoraire != 0) {
         
-        printf("Choix de la formation\n");
-        printf("0 : Quitter\n");
-        printf("1 : Formation x\n");
-        printf("2 : Formation y\n");
+    printf("Choix de la formation\n");
+    printf("0 : Quitter\n");
+    printf("1 : Formation x\n");
+    printf("2 : Formation y\n");
 
+    scanf("%d", &valeurMenuHoraire);
+
+    //Gestion d'erreur
+    while(valeurMenuHoraire < 0 || valeurMenuHoraire > 2) {
+        printf("Valeur incorrecte.\n");
         scanf("%d", &valeurMenuHoraire);
-
-        //Gestion d'erreur
-        while(valeurMenuHoraire < 0 || valeurMenuHoraire > 2) {
-            printf("Valeur incorrecte.\n");
-            scanf("%d", valeurMenuHoraire);
-        }
-
-        if(valeurMenuHoraire == 1) {
-            //Bloc d'instruction pour la formation
-        }
-
-        //Et ainsi de suite
     }
+
+    if(valeurMenuHoraire == 1) {
+        //Bloc d'instruction pour la formation
+        printf("Vous avez choisi 1\n");
+    }
+
+    //Et ainsi de suite
+}
+
+
+int menuGererFormation() {
+    int valeurMenuFormation=-1;
+    int queFaire = 0; //0 signifie ne rien faire. Cette valeur est retournée si l'utilisateur n'a pas besoin de lire/ecrire dans un fichier
+        
+    printf("Choix de la formation\n");
+    printf("0 : Quitter\n");
+    printf("1 : Accéder aux formations\n");
+    printf("2 : Accéder aux Formateurs y\n");
+    printf("Votre choix : ");
+
+    scanf("%d", &valeurMenuFormation);
+
+    //Gestion d'erreur
+    while(valeurMenuFormation < 0 || valeurMenuFormation > 2) {
+        printf("Valeur incorrecte.\n");
+        printf("Votre choix : ");
+        scanf("%d", &valeurMenuFormation);
+    }
+
+    if(valeurMenuFormation == 1) {
+        queFaire = gestionFormation(); 
+        return queFaire;
+        /*Regarde avec gestionFormation() : 
+          Si l'utilisateur a choisi l'option 1, on sait qu'il veut ajouter une formation. Ca implique qu'il doive écrire dans un fichier.
+          On return ici 1 et dans le main on fait écrire toutes les parties d'une structure Formation.
+        */
+    }
+
+    if(valeurMenuFormation == 2) {
+        //TODO : prévoir ce cas
+        //Note : queFaire devra être différent de 0,1,2 et 3 car ces options sont déjà reprises dans le cas 1 et il faut que queFaire soit différent pour chaque
+        //option regroupée dans le main -->
+        //if(valeurMenu == X) {}
+    }
+}
+
+//Est appelée depuis menuGererFormation()
+int gestionFormation() {
+    int queFaire = 0;
+    int menu;
+    printf("0 : Quitter\n");
+    printf("1. Ajouter une formation\n");
+    printf("2. Modifier une formation\n");
+    printf("3. Afficher la liste des formations");
+
+    printf("Votre choix : ");
+    scanf("%d", &menu);
+
+    while(menu < 0 || menu > 3) {
+        printf("Valeur incorrecte.\n");
+        printf("Votre choix : ");
+        scanf("%d", &menu);
+    }
+
+    queFaire = menu;
+    return queFaire;
 }
 
